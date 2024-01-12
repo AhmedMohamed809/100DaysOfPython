57c8ac6861e871a981cea64cfb6453705dab428d
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2024-01-12 14:43:41 +0000

    task1

---
+++ Day2/task1.py
@@ -0,0 +1,8 @@
+two_digit_number = input("Ener your Numbers")
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+frist_num=two_digit_number[0]
+
+sec_num = two_digit_number[1]
+print(int(frist_num) + int(sec_num) )
